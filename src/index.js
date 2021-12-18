a927a02503290bc763f142be9634a316267ce157
Faruq Bakare <bakarefaruq.a@gmail.com>
2021-12-18 15:00:50 +0100

    Clean up Fonts setup

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './fonts/Axiforma/Axiforma-Bold.ttf';
+import './fonts/Axiforma/Axiforma-Medium.ttf';
+import './fonts/Axiforma/Axiforma-Regular.ttf';
+import './fonts/Axiforma/Axiforma-SemiBold.ttf';
 
 ReactDOM.render(
   <React.StrictMode>
